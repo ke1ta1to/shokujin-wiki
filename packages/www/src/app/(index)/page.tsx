1e2b9e9b9a873fab70da89d0bbcc2d4430e8ce3a
Keita Ito <keitaito@keitaito.net>
2025-05-17 22:11:00 +0900

    chore: muiの導入

--- packages/www/src/app/(index)/page.tsx
+++ packages/www/src/app/(index)/page.tsx
@@ -2,6 +2,7 @@ export default async function IndexPage() {
   return (
     <div>
       <h1>Index Page</h1>
+      <p>日本語テスト</p>
     </div>
   );
 }
--- packages/www/src/app/layout.tsx
+++ packages/www/src/app/layout.tsx
@@ -1,15 +1,35 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import type { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
 import type { ReactNode } from "react";
+
+import { theme } from "@/theme";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "食神Wiki",
 };
 
+const notoSansJp = Noto_Sans_JP({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-noto-sans-jp",
+});
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="ja">
-      <body>{children}</body>
+    <html lang="ja" className={notoSansJp.variable}>
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
