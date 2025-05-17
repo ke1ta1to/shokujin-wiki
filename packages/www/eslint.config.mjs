399bc8cbb182cee1461e53b0056af41d4f611b17
Keita Ito <keitaito@keitaito.net>
2025-05-17 20:38:44 +0900

    chore: ESLintとPrettierの設定を追加

--- packages/www/eslint.config.mjs
+++ packages/www/eslint.config.mjs
@@ -6,7 +6,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
     rules: {
       "import/order": [
         "error",
