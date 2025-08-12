68fcb15459b9c260de08c6e2d1bd43e5528de535
Keita Ito <keitaito@keitaito.net>
2025-08-13 04:24:11 +0900

    feat: アップロード用のAWS構成

--- packages/infra/eslint.config.mjs
+++ packages/infra/eslint.config.mjs
@@ -2,9 +2,13 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { globalIgnores } from "eslint/config";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  eslintConfigPrettier,
+  globalIgnores(["cdk.out"]),
 );
