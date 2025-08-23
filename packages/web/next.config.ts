ba85841d7f4957cdfc8bbbf764e1d307a4a1231e
Keita Ito <keitaito@keitaito.net>
2025-08-24 02:31:42 +0900

    feat: 画像設定を最適化し、リモートパターンを削除

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -2,17 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns:
-      process.env.NODE_ENV === "development"
-        ? [
-            {
-              protocol: "https",
-              hostname: "**",
-            },
-          ]
-        : [new URL(process.env.NEXT_PUBLIC_UPLOADED_CLOUDFRONT_URL as string)],
+    unoptimized: true,
   },
-  serverExternalPackages: ["@supabase/supabase-js"],
 };
 
 export default nextConfig;
