a923e565416e3abc0270929d4c3c4cd4f4c7ee15
Keita Ito <keitaito@keitaito.net>
2025-08-27 01:56:18 +0900

    feat: タイトルの変更

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -1,10 +1,19 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 
 import { AppLayout } from "@/components/app-layout";
 import { theme } from "@/theme";
+
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - 食神Wiki",
+    default: "食神Wiki",
+  },
+};
 
 const notoSansJp = Noto_Sans_JP({
   weight: ["300", "400", "500", "700"],
