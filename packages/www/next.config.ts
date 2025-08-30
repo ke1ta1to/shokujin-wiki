ab19542a07e20b4325ac88d601bacd0721f48251
Keita Ito <keitaito@keitaito.net>
2025-08-30 15:27:51 +0900

    fix: add webpack plugins

--- packages/www/next.config.ts
+++ packages/www/next.config.ts
@@ -1,8 +1,16 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   },
 };
 
---
+++ packages/www/src/types/index.d.ts
@@ -0,0 +1,4 @@
+declare module "@prisma/nextjs-monorepo-workaround-plugin" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export const PrismaPlugin: any;
+}
