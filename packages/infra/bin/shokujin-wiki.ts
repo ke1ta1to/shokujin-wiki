0be3446226e88781a412335ca6fbcf751d6438b5
Keita Ito <keitaito@keitaito.net>
2025-08-30 21:32:42 +0900

    feat: add rewrites for AWS CloudFront uploads (#19)
    * feat: add rewrites for AWS CloudFront uploads
    
    * fix: cdk構成の修正

--- packages/infra/bin/shokujin-wiki.ts
+++ packages/infra/bin/shokujin-wiki.ts
@@ -2,30 +2,21 @@
 import * as cdk from "aws-cdk-lib";
 
 import z from "zod";
-import { ShokujinWikiAppStack } from "../lib/shokujin-wiki-app-stack";
 import { ShokujinWikiStack } from "../lib/shokujin-wiki-stack";
 
 const app = new cdk.App();
 
-const mode = app.node.tryGetContext("mode") || "app";
-
-if (mode === "app") {
-  const environmentSchema = z.enum(["development", "production"]);
-  const environment = app.node.tryGetContext("environment");
-  if (!environmentSchema.safeParse(environment).success) {
-    throw new Error(
-      'Invalid environment. Please specify "-c environment=development" or "-c environment=production".',
-    );
-  }
-
-  new ShokujinWikiAppStack(
-    app,
-    `ShokujinWikiAppStack${capitalize(environment)}`,
-    { environment },
+const environmentSchema = z.enum(["development", "production"]);
+const environment = app.node.tryGetContext("environment");
+if (!environmentSchema.safeParse(environment).success) {
+  throw new Error(
+    'Invalid environment. Please specify "-c environment=development" or "-c environment=production".',
   );
-} else if (mode === "default") {
-  new ShokujinWikiStack(app, "ShokujinWikiStack");
 }
+
+new ShokujinWikiStack(app, `ShokujinWikiStack${capitalize(environment)}`, {
+  environment,
+});
 
 function capitalize(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
--- packages/www/next.config.ts
+++ packages/www/next.config.ts
@@ -12,6 +12,14 @@ const nextConfig: NextConfig = {
 
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: "/uploads/:path*",
+        destination: `${process.env.AWS_CLOUDFRONT_URL}/uploads/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
