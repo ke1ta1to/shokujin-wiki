a4ff9f6b5bd707af470ff83d7a230b2d4d02a29b
Keita Ito <keitaito@keitaito.net>
2025-08-30 21:22:12 +0900

    fix: cdk構成の修正

--- packages/infra/lib/shokujin-wiki-stack.ts
+++ packages/infra/lib/shokujin-wiki-stack.ts
@@ -1,14 +1,62 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as cloudfront_origins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as s3 from "aws-cdk-lib/aws-s3";
+
+interface ShokujinWikiStackProps extends cdk.StackProps {
+  environment: "development" | "production";
+}
 
 export class ShokujinWikiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ShokujinWikiStackProps) {
     super(scope, id, props);
 
-    new iam.User(this, "ShokujinWikiUser", {
-      userName: "shokujin-wiki-uploads-user",
+    const isProduction = props.environment === "production";
+
+    const uploadsBucket = new s3.Bucket(this, "UploadsBucket", {
+      bucketName: `shokujin-wiki-uploads-${props.environment}-${this.account}`,
+      removalPolicy: isProduction
+        ? cdk.RemovalPolicy.RETAIN
+        : cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: !isProduction,
+      cors: [
+        {
+          allowedMethods: [s3.HttpMethods.POST],
+          allowedOrigins: ["*"],
+        },
+      ],
     });
+
+    const distribution = new cloudfront.Distribution(
+      this,
+      "UploadsDistribution",
+      {
+        defaultBehavior: {
+          origin:
+            cloudfront_origins.S3BucketOrigin.withOriginAccessControl(
+              uploadsBucket,
+            ),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      },
+    );
+
+    new cdk.CfnOutput(this, "UploadsBucketName", {
+      value: uploadsBucket.bucketName,
+    });
+
+    new cdk.CfnOutput(this, "UploadsDistributionDomainName", {
+      value: distribution.domainName,
+    });
+
+    const uploadsUser = new iam.User(this, "ShokujinWikiUser", {
+      userName: `shokujin-wiki-uploads-user-${props.environment}`,
+    });
+
+    uploadsBucket.grantWrite(uploadsUser);
   }
 }
