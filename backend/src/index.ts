cab16e1b362f04c43462f4eda6b9c84358dc93f8
Keita Ito <keitaito@keitaito.net>
2025-12-10 12:33:02 +0900

    feat: basePathにapiを追加

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,10 @@
-import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 
 import reviews from "./routes/reviews.js";
 
-const app = new Hono();
+const app = new Hono().basePath("/api");
 
 app.use(logger());
 
@@ -14,9 +13,5 @@ app.use("/swagger-ui", serveStatic({ path: "./openapi/swagger-ui.html" }));
 app.use("/openapi.yaml", serveStatic({ path: "./openapi/openapi.yaml" }));
 
 app.route("/reviews", reviews);
-
-serve({ fetch: app.fetch, port: 3000 }, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`);
-});
 
 export default app;
--- backend/test/review.test.ts
+++ backend/test/review.test.ts
@@ -2,21 +2,21 @@ import { describe, expect, test } from "vitest";
 
 import app from "../src/index.js";
 
-describe("GET /reviews", () => {
+describe("GET /api/reviews", () => {
   test("should return a list of reviews", async () => {
-    const res = await app.request("/reviews");
+    const res = await app.request("/api/reviews");
     const json = await res.json();
     expect(res.status).toBe(200);
     expect(Array.isArray(json)).toBe(true);
   });
 });
 
-describe("POST /reviews", () => {
+describe("POST /api/reviews", () => {
   test("should create a new review", async () => {
     const newReview = {
       comment: "Hello, this is a test review.",
     };
-    const res = await app.request("/reviews", {
+    const res = await app.request("/api/reviews", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
