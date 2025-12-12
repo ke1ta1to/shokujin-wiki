ff0e3862eeab959e39d2937c1a2228bc001c79e2
Keita Ito <keitaito@keitaito.net>
2025-12-13 05:29:33 +0900

    feat: add tailwindcss

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -3,7 +3,7 @@ import { RpcSample } from "@/components/rpc-sample";
 export default async function IndexPage() {
   return (
     <>
-      <h1>Shokujin Wikiへようこそ</h1>
+      <h1 className="text-3xl font-bold underline">Shokujin Wikiへようこそ</h1>
       <RpcSample />
     </>
   );
