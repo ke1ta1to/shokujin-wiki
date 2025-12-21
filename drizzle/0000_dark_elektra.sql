6c1bd032c67d2b7427fb86e868c1f4341a3b88d7
Keita Ito <keitaito@keitaito.net>
2025-12-22 01:59:37 +0900

    chore: プロジェクトの削除

--- drizzle/0000_dark_elektra.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE "reviews" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"content" text
-);
--- src/db/schema.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
-
-export const reviewsTable = pgTable("reviews", {
-  id: serial("id").primaryKey(),
-  content: text("content"),
-});
