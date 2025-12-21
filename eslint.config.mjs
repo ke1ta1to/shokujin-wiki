afd5f247a72cf63b03a51c13dec91e65b7a23b8a
Keita Ito <keitaito@keitaito.net>
2025-12-22 02:24:35 +0900

    chore: add eslint import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,28 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+  {
+    files: ["**/*.{ts,tsx,mjs}"],
+    rules: {
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+        },
+      ],
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
